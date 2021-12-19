489f47fbd9f87beba005bb33233989e1bbe77cfb
The Lord <egortuisov@gmail.com>
2021-12-19 15:59:31 +0500

    чуть больше комментов

--- running.py
+++ running.py
@@ -3,13 +3,17 @@
 
 import pygame
 from const import service, sizes
-import start_menu
+import start_menu, main_menu, level_play, upgrading
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(sizes.WINDOW_SIZE)
-    actions = {service.START_MENU: start_menu.run}
+    actions = {service.START_MENU: start_menu.run,
+               service.MAIN_MENU: main_menu.run,
+               service.LEVEL_PLAY: level_play.run,
+               service.UPGRADING: upgrading.run}
+    # функция run обрабатывает один кадр и возвращает какая активность будет следующей
     cur_action = service.START_MENU
     while cur_action != service.QUIT:
         cur_action = actions[cur_action]()
