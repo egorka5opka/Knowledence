91e09b01c2624ad70a0fd345a4ecb3f31e6f8d5a
Egorka5opka <egortuisov@gmail.com>
2022-01-18 12:47:55 +0500

    координаты уровней в главном меню + изменения в функциях run

--- main_menu.py
+++ main_menu.py
@@ -1,12 +1,12 @@
 import pygame
 from const import service
 from const.file_paths import MAP_BACKGROUND, SETTINGS_BUTTON, ENCYCLOPEDIA_BUTTON, UPGRADING_BUTTON, HOME_BUTTON, \
-    COMPLETE_LEVEL, INCOMPLETE_LEVEL, LAUNCH_BACKGROUND, PLAY_BUTTON, CLOSE_BUTTON, LEVEL_BACKGROUND
+    COMPLETE_LEVEL, INCOMPLETE_LEVEL, LAUNCH_BACKGROUND, PLAY_BUTTON, CLOSE_BUTTON
 from const.sizes import WINDOW_WIDTH, WINDOW_HEIGHT
 from tools.methods import load_image
 from tools.classes import Button
 import csv
-from level_play import set_level
+
 
 def run(screen, *args, **kwargs):
     all_sprites = pygame.sprite.Group()
@@ -19,21 +19,18 @@ def run(screen, *args, **kwargs):
             coords_levels = [(int(i[0]), int(i[1])) for i in reader]
     except Exception as e:
         print("Не удалось загрузить уровни: " + str(e))
+    kol = open("data/progress.txt", mode='r')
+    kol = int(kol.readline())
 
-    count = open("data/progress.txt", 'r')
-    count = int(count.readline())
+    fon = pygame.transform.scale(load_image(MAP_BACKGROUND), (WINDOW_WIDTH, WINDOW_HEIGHT))
+    settings_button = Button(SETTINGS_BUTTON, all_sprites, button_sprites, 1380, 10)
+    home_button = Button(HOME_BUTTON, all_sprites, button_sprites, 1462, 10)
+    encyclopedia_button = Button(ENCYCLOPEDIA_BUTTON, all_sprites, button_sprites, 1140, 640)
+    upgrading_button = Button(UPGRADING_BUTTON, all_sprites, button_sprites, 1365, 640)
 
-    # fon = pygame.transform.scale(load_image(MAP_BACKGROUND), (WINDOW_WIDTH, WINDOW_HEIGHT))
-    fon = load_image(MAP_BACKGROUND)
-    settings_button = Button(SETTINGS_BUTTON,  1380, 10, all_sprites, button_sprites)
-    home_button = Button(HOME_BUTTON, 1462, 10, all_sprites, button_sprites)
-    encyclopedia_button = Button(ENCYCLOPEDIA_BUTTON, 1140, 640, all_sprites, button_sprites)
-    upgrading_button = Button(UPGRADING_BUTTON, 1365, 640, all_sprites, button_sprites)
-
-    for i in range(count):
-        Button(COMPLETE_LEVEL,  coords_levels[i][0], coords_levels[i][1], all_sprites, levels_sprites)
-    if count != 10:
-        Button(INCOMPLETE_LEVEL, coords_levels[count][0], coords_levels[count][1], all_sprites, levels_sprites)
+    for i in range(kol):
+        Button(COMPLETE_LEVEL, all_sprites, levels_sprites, coords_levels[i][0], coords_levels[i][1])
+    Button(INCOMPLETE_LEVEL, all_sprites, levels_sprites, coords_levels[kol][0], coords_levels[kol][1])
 
     screen.blit(fon, (0, 0))
     all_sprites.draw(screen)
@@ -50,26 +47,23 @@ def run(screen, *args, **kwargs):
                 elif encyclopedia_button.get_click(event.pos[0], event.pos[1]):
                     pass
                 elif upgrading_button.get_click(event.pos[0], event.pos[1]):
-                    return service.UPGRADING
-                for ind, spr in enumerate(levels_sprites):
+                    pass
+                for spr in levels_sprites:
                     if spr.get_click(event.pos[0], event.pos[1]):
-                        return launch(screen, ind)
+                        return launch(screen)
         pygame.display.flip()
 
-
-def launch(screen, level):
+def launch(screen):
     SELF_WIDTH, SELF_HEIGHT = 742, 447
 
     all_sprites = pygame.sprite.Group()
     button_sprites = pygame.sprite.Group()
 
-    background = pygame.transform.scale(load_image(LAUNCH_BACKGROUND), (SELF_WIDTH, SELF_HEIGHT))
-    map_of_level = pygame.transform.scale(load_image(LEVEL_BACKGROUND.format(level)), (465, 240))
-    play_button = Button(PLAY_BUTTON, (WINDOW_WIDTH - 95) // 2, 550, all_sprites, button_sprites)
-    close_button = Button(CLOSE_BUTTON,  1090, 190, all_sprites, button_sprites)
+    fon = pygame.transform.scale(load_image(LAUNCH_BACKGROUND), (SELF_WIDTH, SELF_HEIGHT))
+    play_button = Button(PLAY_BUTTON, all_sprites, button_sprites, (WINDOW_WIDTH - 95) // 2, 550)
+    close_button = Button(CLOSE_BUTTON, all_sprites, button_sprites, 1090, 190)
 
-    screen.blit(background, ((WINDOW_WIDTH - SELF_WIDTH) // 2, (WINDOW_HEIGHT - SELF_HEIGHT) // 2))
-    screen.blit(map_of_level, ((WINDOW_WIDTH - SELF_WIDTH) // 2 + 30, (WINDOW_HEIGHT - SELF_HEIGHT) // 2 + 30))
+    screen.blit(fon, ((WINDOW_WIDTH - SELF_WIDTH) // 2, (WINDOW_HEIGHT - SELF_HEIGHT) // 2))
     all_sprites.draw(screen)
 
     while True:
@@ -78,7 +72,6 @@ def launch(screen, level):
                 return service.QUIT
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if play_button.get_click(event.pos[0], event.pos[1]):
-                    set_level(level)
                     return service.LEVEL_PLAY
                 elif close_button.get_click(event.pos[0], event.pos[1]):
                     return service.MAIN_MENU
--- upgrading.py
+++ upgrading.py
@@ -1,51 +1,9 @@
 import pygame
 from const import service
-from const.sizes import WINDOW_WIDTH, WINDOW_HEIGHT
-from const.file_paths import UPGRADING_BACKGROUND, CLOSE_BUTTON, UPGRADE_BUTTON
-from tools.methods import load_image
-from tools.classes import Button
-import csv
 
 
 def run(screen,  *args, **kwargs):
-    all_sprites = pygame.sprite.Group()
-    button_sprites = pygame.sprite.Group()
-    upgrading_button = pygame.sprite.Group()
-
-    try:
-        with open("data/upgrading_coords.csv") as fin:
-            reader = csv.reader(fin, delimiter=";")
-            coords_upgrading = [(int(i[0]), int(i[1])) for i in reader]
-    except Exception as e:
-        print("Не удалось загрузить улучшения: " + str(e))
-
-    background = pygame.transform.scale(load_image(UPGRADING_BACKGROUND), (WINDOW_WIDTH, 1719))
-    close_button = Button(CLOSE_BUTTON,  1490, 20, all_sprites, button_sprites)
-    for i in range(len(coords_upgrading)):
-        Button(UPGRADE_BUTTON,  coords_upgrading[i][0], coords_upgrading[i][1], all_sprites, upgrading_button)
-
-    up_coord = 0
-    screen.blit(background, (0, up_coord))
-    all_sprites.draw(screen)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return service.QUIT
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 4:
-                    # колесо вверх
-                    up_coord = min(0, up_coord + 50)
-                    for ind, spr in enumerate(upgrading_button):
-                        spr.rect.top = min(coords_upgrading[ind][1], spr.rect.top + 50)
-                elif event.button == 5:
-                    up_coord = max(-919, up_coord - 50)
-                    for ind, spr in enumerate(upgrading_button):
-                        spr.rect.top = max(-919 + coords_upgrading[ind][1], spr.rect.top - 50)
-                if close_button.get_click(event.pos[0], event.pos[1]):
-                    return service.MAIN_MENU
-
-        screen.blit(background, (0, up_coord))
-        all_sprites.draw(screen)
-        pygame.display.flip()
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return service.QUIT
+    return service.UPGRADING
