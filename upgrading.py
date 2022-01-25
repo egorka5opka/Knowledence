c6d0dd1b90d75d5e6534dd474083207c6767f059
petrenkosofya <Sofi20102005@gmail.com>
2022-01-25 14:06:53 +0500

    сделана часть главного меню

--- upgrading.py
+++ upgrading.py
@@ -1,9 +1,19 @@
 import pygame
 from const import service
+from const.sizes import WINDOW_WIDTH, WINDOW_HEIGHT
+from const.file_paths import LAUNCH_BACKGROUND
+from tools.methods import load_image
+from tools.classes import Button
 
 
 def run(screen,  *args, **kwargs):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return service.QUIT
-    return service.UPGRADING
+    background = pygame.transform.scale(load_image(LAUNCH_BACKGROUND), (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    screen.blit(background, (0, 0))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return service.QUIT
+        pygame.display.flip()
+
