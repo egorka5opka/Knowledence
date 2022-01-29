2401807d6ecc2dcdb736c12b2fb27a1c8e4b074c
The Lord <egortuisov@gmail.com>
2022-01-29 19:38:19 +0500

    теперь вся информация о врагах и уровнях берётся из файлов, можно сказать что класс Enemy закончен

--- level_play.py
+++ level_play.py
@@ -12,23 +12,18 @@ def run(screen,  *args, **kwargs):
     all_sprites = pygame.sprite.Group()
     tower_sprites = pygame.sprite.Group()
     level = 0
-    twist_points, tower_places_sprites, background = load_level(all_sprites)
-    if twist_points == None:
+    waves, tower_places_sprites, background = load_level(all_sprites)
+    if not waves:
+        print("Не удалось загрузить уровень")
         return service.MAIN_MENU
     running = service.LEVEL_PLAY
-    monster = enemies.Enemy(twist_points, all_sprites)
     clock = pygame.time.Clock()
     while running == service.LEVEL_PLAY:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = service.QUIT
                 break
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                monster.impact(event.button - 2, (service.VELOCITY_BUFF, 0.1, 5000))
-        monster.update(clock.tick())
         all_sprites.draw(screen)
-        for i in range(len(twist_points) - 1):
-            pygame.draw.line(screen, (0, 0, 0), twist_points[i], twist_points[i + 1], 5)
         pygame.display.flip()
     return running
 
@@ -39,21 +34,28 @@ def set_level(x):
 
 
 def load_level(all_sprites):
-    background = classes.Background(file_paths.LEVEL_BACKGROUND.format(level), all_sprites)
-    twist_points = []
-    tower_places_sprites = pygame.sprite.Group()
     try:
+        ways = []
+        tower_places_sprites = pygame.sprite.Group()
+        waves = []
         fin = open(file_paths.LEVEL_DATA.format(level))
         reader = csv.reader(fin, delimiter=";")
-        cnt_points = int(reader.__next__()[0])
-        for _ in range(cnt_points):
-            coords = reader.__next__()
-            twist_points.append((int(coords[0]), int(coords[1])))
+        bckg_file = reader.__next__()[0]
+        background = classes.Background(bckg_file, all_sprites)
         cnt_places = int(reader.__next__()[0])
         for _ in range(cnt_places):
             coords = reader.__next__()
             towers.TowerPlace((int(coords[0]), int(coords[1])), all_sprites, tower_places_sprites)
+        cnt_ways = int(reader.__next__()[0])
+        for _ in range(cnt_ways):
+            twist_points = []
+            cnt_points = int(reader.__next__()[0])
+            for __ in range(cnt_points):
+                coords = reader.__next__()
+                twist_points.append((int(coords[0]), int(coords[1])))
+            ways.append(twist_points)
+        fin.close()
     except Exception as e:
         print("Не удалось открыть файл data/" + file_paths.LEVEL_DATA.format(level), e)
         return None, None, None
-    return twist_points, tower_places_sprites, background
+    return waves, tower_places_sprites, background
