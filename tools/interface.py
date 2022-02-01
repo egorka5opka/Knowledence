2216976fab655a0a53f79965844abffe419e771b
Egorka5opka <egortuisov@gmail.com>
2022-02-01 18:44:56 +0500

    начало проработки интерфейса

--- tools/interface.py
+++ tools/interface.py
@@ -4,19 +4,21 @@ from tools import classes, methods
 
 
 class Lives(pygame.sprite.Sprite):
-    image = methods.load_image(file_paths.LIVES_IMAGE)
+    image = methods.load_image("resources.png")
 
     def __init__(self, points, *args):
         super().__init__(*args)
         self.points = points
         self.rect = self.image.get_rect()
-        self.rect = self.rect.move(sizes.LIVES_POS)
+        self.rect = self.rect.move(1350, 0)
 
     def __iadd__(self, other):
         self.points -= other
+        return self
 
     def __isub__(self, other):
         self.points -= other
+        return self
 
     def get_points(self):
         return self.points
